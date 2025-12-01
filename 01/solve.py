967b4f8d348214dedc3c8908a7ced3cddd3e8278
Brian Starkey <stark3y@gmail.com>
2025-12-01 11:28:42 +0000

    01: part2

--- 01/solve.py
+++ 01/solve.py
@@ -2,6 +2,7 @@ import sys
 
 pos = 50
 part1 = 0
+part2 = 0
 N_POS = 100
 dirs = {
     "L": -1,
@@ -13,9 +14,21 @@ with open(sys.argv[1]) as f:
         d = dirs[line[0]]
         n = int(line[1:])
 
-        pos += d * n
-        pos = pos % N_POS
+        clicks = d * n
+        full_rots = abs(clicks) // N_POS
+        mod = abs(clicks) % N_POS
 
+        # The "pos != 0" clause is a little messy here, there might
+        # be a neater way
+        if d < 0 and pos != 0 and mod >= pos:
+            part2 += 1
+        elif d > 0 and pos != 0 and mod >= (N_POS - pos):
+            part2 += 1
+        part2 += full_rots
+
+        pos += clicks
+        pos = pos % N_POS
         if pos == 0:
             part1 += 1
 print(part1)
+print(part2)

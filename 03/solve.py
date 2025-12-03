d337e38711554a9f6030000691ee67264db50ced
Brian Starkey <stark3y@gmail.com>
2025-12-03 08:52:22 +0000

    03: part 2

--- 03/solve.py
+++ 03/solve.py
@@ -1,12 +1,27 @@
 import sys
 
+def calculate(line, part2 = False):
+    ndigits = 12 if part2 else 2
+    number = ""
+    idx = 0
+    for n in range(ndigits):
+        # Find the largest up-to the last "tail" digits
+        tail = ndigits - n - 1
+        # Not sure if there's a better way to make [:-0] work
+        if tail > 0:
+            sub = line[idx:-(ndigits - n - 1)]
+        else:
+            sub = line[idx:]
+        largest = sorted(sub, reverse=True)[0]
+        idx += sub.find(largest) + 1
+        number += largest
+    return int(number)
+
 part1 = 0
+part2 = 0
 with open(sys.argv[1]) as f:
     for line in map(str.strip, f):
-        largest = sorted(line[:-1], reverse=True)[0]
-        idx = line.find(largest)
-        second = sorted(line[idx+1:], reverse=True)[0]
-        number = int(largest + second)
-
-        part1 += number
+        part1 += calculate(line)
+        part2 += calculate(line, True)
 print(part1)
+print(part2)

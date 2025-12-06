51b93df7b7c7e08817c1fff8f083b84d0b3cd3aa
Brian Starkey <stark3y@gmail.com>
2025-12-06 11:07:38 +0000

    06: neater part 2
    Occurred to me in the shower

--- 06/solve.py
+++ 06/solve.py
@@ -1,6 +1,6 @@
 import sys
 
-# Just load the input into a transposed grid
+# Just load the input into a transposed grid delimeted by spaces
 ncols = None
 with open(sys.argv[1]) as f:
     for line in map(str.strip, f):
@@ -17,42 +17,45 @@ def product(vs):
         result *= v
     return result
 
-operations = {
+operators = {
     "+": sum,
     "*": product,
 }
 part1 = 0
 for problem in problems:
-    part1 += operations[problem[-1]](map(int, problem[:-1]))
+    part1 += operators[problem[-1]](map(int, problem[:-1]))
 print(part1)
 
 
-# Eric sold us a lie, so part2 is completely different - load the whole file
-# into a grid, use the operators to find the columns.
+# Eric sold us a lie, so part2 is completely different
+# load the whole file into a grid and transpose it character-wise
 grid = []
 with open(sys.argv[1]) as f:
     for line in f:
         grid.append(line.strip("\n"))
 
-def find_operators(line):
-    operators = []
-    for i, c in enumerate(line):
-        if c != " ":
-            operators.append((i, c))
-    operators.append((len(line) + 1, ""))
-
-    return operators
-
-operators = find_operators(grid[-1])
-
 part2 = 0
-for i, (start, op) in enumerate(operators[:-1]):
-    end = operators[i+1][0]
-    numbers = ["" for _ in range(end - start - 1)]
-    for row in grid[:-1]:
-        for n, col in enumerate(range(start, end - 1)):
-            c = row[col]
-            if c != " ":
-                numbers[n] += c
-    part2 += operations[op](map(int, numbers))
+numbers = []
+op = None
+# Assume all lines are padded
+for col in range(len(grid[0])):
+    line = ""
+    for row in range(len(grid)):
+        char = grid[row][col]
+        if char in operators:
+            op = char
+        elif char != " ":
+            line += char
+
+    # Line is now either empty (separates problems), or contains
+    # a number for the current problem
+    if line == "":
+        part2 += operators[op](map(int, numbers))
+        numbers = []
+        op = None
+    else:
+        numbers.append(line)
+
+# Finish the final problem
+part2 += operators[op](map(int, numbers))
 print(part2)

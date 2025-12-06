70474f552c91e1980df84b0a3c29a33602015161
Brian Starkey <stark3y@gmail.com>
2025-12-06 09:28:20 +0000

    06: part 2

--- 06/solve.py
+++ 06/solve.py
@@ -1,5 +1,6 @@
 import sys
 
+# Just load the input into a transposed grid
 ncols = None
 with open(sys.argv[1]) as f:
     for line in map(str.strip, f):
@@ -16,11 +17,42 @@ def product(vs):
         result *= v
     return result
 
-operators = {
+operations = {
     "+": sum,
     "*": product,
 }
 part1 = 0
 for problem in problems:
-    part1 += operators[problem[-1]](map(int, problem[:-1]))
+    part1 += operations[problem[-1]](map(int, problem[:-1]))
 print(part1)
+
+
+# Eric sold us a lie, so part2 is completely different - load the whole file
+# into a grid, use the operators to find the columns.
+grid = []
+with open(sys.argv[1]) as f:
+    for line in f:
+        grid.append(line.strip("\n"))
+
+def find_operators(line):
+    operators = []
+    for i, c in enumerate(line):
+        if c != " ":
+            operators.append((i, c))
+    operators.append((len(line) + 1, ""))
+
+    return operators
+
+operators = find_operators(grid[-1])
+
+part2 = 0
+for i, (start, op) in enumerate(operators[:-1]):
+    end = operators[i+1][0]
+    numbers = ["" for _ in range(end - start - 1)]
+    for row in grid[:-1]:
+        for n, col in enumerate(range(start, end - 1)):
+            c = row[col]
+            if c != " ":
+                numbers[n] += c
+    part2 += operations[op](map(int, numbers))
+print(part2)

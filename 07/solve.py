28968e5476491a241864f577fc5659ec014fd2ac
Brian Starkey <stark3y@gmail.com>
2025-12-07 21:57:43 +0000

    07: part 2
    Not too bad as there's only one beam at a time - simple recursion +
    memoize

--- 07/solve.py
+++ 07/solve.py
@@ -1,3 +1,4 @@
+from functools import cache
 import sys
 
 part1 = 0
@@ -19,3 +20,23 @@ with open(sys.argv[1]) as f:
                 new_wave.add(idx + 1)
         wave = new_wave
 print(part1)
+
+@cache
+def split(beam, lines):
+    if len(lines) == 0:
+        return 1
+
+    if lines[0][beam] == "^":
+        left = split(beam - 1, lines[1:])
+        right = split(beam + 1, lines[1:])
+        return left + right
+    return split(beam, lines[1:])
+
+lines = []
+with open(sys.argv[1]) as f:
+    for line in map(str.strip, f):
+        lines.append(line)
+
+beam = lines[0].find("S")
+part2 = split(beam, tuple(lines[1:]))
+print(part2)

0f152330a7a2d0dea561a89c9025ec7b91f3a152
Brian Starkey <stark3y@gmail.com>
2025-12-08 21:00:11 +0000

    08: part 1
    Fairly naive, straightforward implementation.
    
    The only optimisation I've made is to track the actual circuits
    separately from the mapping from node->circuit, to save a consolidation
    step at the end.

---
+++ 08/solve.py
@@ -0,0 +1,51 @@
+import heapq
+import sys
+
+from collections import defaultdict
+
+boxes = []
+with open(sys.argv[1]) as f:
+    for line in map(str.strip, f):
+        x, y, z = map(int, line.split(","))
+        boxes.append((x, y, z))
+
+# Skip the sqrt...
+def distance(a, b):
+    return (a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2
+
+distances = []
+seen = set()
+for i, a in enumerate(boxes):
+    for ii, b in enumerate(boxes[i:]):
+        j = i + ii
+        if i == j:
+            continue
+        d = distance(a, b)
+        heapq.heappush(distances, (d, i, j))
+
+circuits = [
+    set([x]) for x in range(len(boxes))
+]
+circuit_idxs = {
+    x: x for x in range(len(boxes))
+}
+
+NCONNS = 1000
+for i in range(NCONNS):
+    _, a, b = heapq.heappop(distances)
+    ac = circuits[circuit_idxs[a]]
+    bc = circuits[circuit_idxs[b]]
+
+    if ac is bc:
+        continue
+
+    # Merge them, remove all references to bc, and wipe it
+    ac.update(bc)
+    for c in bc:
+        circuit_idxs[c] = circuit_idxs[a]
+    bc.clear()
+
+sizes = sorted(map(len, circuits), reverse=True)
+
+part1 = sizes[0] * sizes[1] * sizes[2]
+print(part1)

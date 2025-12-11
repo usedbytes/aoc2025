8cd9c1f986f79bad7e5efeed26ea84fc913c5e50
Brian Starkey <stark3y@gmail.com>
2025-12-11 08:51:29 +0000

    11: part 2

--- 11/solve.py
+++ 11/solve.py
@@ -1,4 +1,5 @@
 import sys
+from functools import cache
 
 conns = {}
 with open(sys.argv[1]) as f:
@@ -10,16 +11,39 @@ with open(sys.argv[1]) as f:
         conns[output] = inputs
 
 # Assume there are no loops, and do a simple DFS
-# Could add @cache if needed
-def search(start):
+@cache
+def search_p1(start):
     ins = conns[start]
     paths = 0
     for i in ins:
         if i == "out":
             paths += 1
         else:
-            paths += search(i)
+            paths += search_p1(i)
     return paths
 
-part1 = search("you")
+part1 = search_p1("you")
 print(part1)
+
+# Just hard-coding two parameters is easier than messing around
+# with a cacheable datastructure and copying it
+@cache
+def search_p2(start, fft, dac):
+    ins = conns[start]
+    paths = 0
+
+    if start == "fft":
+        fft = True
+    if start == "dac":
+        dac = True
+
+    for i in ins:
+        if i == "out":
+            if (fft and dac):
+                paths += 1
+        else:
+            paths += search_p2(i, fft, dac)
+    return paths
+
+part2 = search_p2("svr", False, False)
+print(part2)

5b5302d2f6608b6f6955ceed6491013ff600dc09
Brian Starkey <stark3y@gmail.com>
2025-12-14 19:50:51 +0000

    10: refactor

--- 10/solve.py
+++ 10/solve.py
@@ -1,6 +1,37 @@
 import heapq
 import sys
 
+def do_line_part1(target, buttons):
+    # Do a BFS, keep applying XOR until you reach
+    # the solution
+
+    toggles = []
+    for b in buttons:
+        mask = 0
+        for v in b:
+            mask |= (1 << v)
+        toggles.append(mask)
+
+    heap = []
+    # Initial state is we push each button once
+    # Heap stores (pushes, state)
+    for mask in toggles:
+        heapq.heappush(heap, (1, mask))
+
+    # No point in revisiting the same states
+    seen = set()
+    while True:
+        pushes, state = heapq.heappop(heap)
+        if state == target:
+            return pushes
+
+        if state in seen:
+            continue
+
+        seen.add(state)
+        for mask in toggles:
+            heapq.heappush(heap, (pushes + 1, state ^ mask))
+
 part1 = 0
 with open(sys.argv[1]) as f:
     for n, line in enumerate(map(str.strip, f)):
@@ -13,34 +44,9 @@ with open(sys.argv[1]) as f:
                 target_lights |= (1 << i)
 
         # Bitmasks per button of the toggled lights
-        buttons_lights = []
+        buttons = []
         for b in buttons_strs:
-            toggles = 0
-            for v in map(int, b[1:-1].split(",")):
-                toggles |= (1 << v)
-            buttons_lights.append(toggles)
+            buttons.append(tuple(map(int, b[1:-1].split(","))))
 
-        # Do a BFS, keep applying XOR until you reach
-        # the solution
-
-        heap = []
-        # Initial state is we push each button once
-        # Heap stores (pushes, state)
-        for b in buttons_lights:
-            heapq.heappush(heap, (1, b))
-
-        # No point in revisiting the same states
-        seen = set()
-        while True:
-            pushes, state = heapq.heappop(heap)
-            if state == target_lights:
-                part1 += pushes
-                break
-
-            if state in seen:
-                continue
-
-            seen.add(state)
-            for b in buttons_lights:
-                heapq.heappush(heap, (pushes + 1, state ^ b))
+        part1 += do_line_part1(target_lights, buttons)
 print(part1)
